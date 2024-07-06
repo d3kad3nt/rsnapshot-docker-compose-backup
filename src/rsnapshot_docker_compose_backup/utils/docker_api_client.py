59e0e969774dffda05019dc3130979344dc88964
Christoph Wildhagen <git@christoph-wildhagen.de>
2024-07-06 23:32:45 +0200

    Fix black formatter issues

--- src/rsnapshot_docker_compose_backup/utils/docker_api_client.py
+++ src/rsnapshot_docker_compose_backup/utils/docker_api_client.py
@@ -67,25 +67,25 @@ class Api:
 
     @staticmethod
     def _parse_response(sock: socket.socket) -> HttpResponse:
-        #Read statusline and headers. I read byte by byte to be sure that not too much is read wich could lead to blocking
+        # Read statusline and headers. I read byte by byte to be sure that not too much is read wich could lead to blocking
         message_start = b""
         while not message_start.endswith(b"\r\n\r\n"):
             message_start = message_start + sock.recv(1)
         lines: list[str] = message_start.decode("utf-8").splitlines()
-        #Parse statusline
+        # Parse statusline
         status_line = lines[0]
         protocol_version = status_line.split(" ")[0]
         status_code = int(status_line.split(" ")[1])
         status_text = " ".join(status_line.split(" ")[2:])
         if status_code >= 400:
             raise ValueError(status_text)
-        #Read headers
+        # Read headers
         headers: dict[str, str] = {}
         for line in lines[1:]:
             split = line.split(":")
             if len(split) == 2:
                 headers[split[0]] = split[1].strip()
-        #read body
+        # read body
         if "Content-Length" in headers:
             body_length = int(headers["Content-Length"])
             body = sock.recv(body_length).decode("utf-8")
