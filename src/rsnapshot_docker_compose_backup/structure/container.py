93554e3ff43170e5d712f64055d2a61b15c48c8d
Christoph Wildhagen <git@christoph-wildhagen.de>
2024-07-07 23:00:29 +0200

    Add equals methods

--- src/rsnapshot_docker_compose_backup/structure/container.py
+++ src/rsnapshot_docker_compose_backup/structure/container.py
@@ -77,3 +77,28 @@ class Container:
         if self.project_name != other.project_name:
             return self.project_name < other.project_name
         return self.service_name < other.service_name
+
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, Container):
+            return False
+        if self.backup_config != other.backup_config:
+            return False
+        if self.container_id != other.container_id:
+            return False
+        if self.container_name != other.container_name:
+            return False
+        if self.docker_compose_file != other.docker_compose_file:
+            return False
+        if self.folder != other.folder:
+            return False
+        if self.image != other.image:
+            return False
+        if self.is_running != other.is_running:
+            return False
+        if self.project_name != other.project_name:
+            return False
+        if self.service_name != other.service_name:
+            return False
+        if self.volumes != other.volumes:
+            return False
+        return True
