f606adebadd94670e35ec9e6bcb1bb7bb13b390f
Christoph Wildhagen <git@christoph-wildhagen.de>
2026-09-17 22:50:23 +0200

    fix: use new Python functions
    The old read_text function was removed in Python 3.13 so I have to use
    other functions

--- tests/test_backup.py
+++ tests/test_backup.py
@@ -116,8 +116,11 @@ def test_not_started_containers(setup_containers: Path) -> None:
 
 
 def load_expected_output(name: str, container_folder: Path) -> str:
-    return resources.read_text(package="tests.output", resource=f"{name}.log").replace(
-        "${{container_folder}}", str(container_folder)
+    return (
+        resources.files("tests.output")
+        .joinpath(f"{name}.log")
+        .read_text(encoding="utf-8")
+        .replace("${{container_folder}}", str(container_folder))
     )
 
 
